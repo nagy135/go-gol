fa3697dbb02c817472adaca30730190af735d8b6
Viktor Nagy <viktor.nagy@365.bank>
2024-03-11 11:30:46 +0100

    feat: init state

--- main.go
+++ main.go
@@ -3,29 +3,41 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"time"
+	// "time"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
+var i = 0
+var memory = make(map[int][][]int)
 
-	ctx := req.Context()
-	fmt.Println("server: hello handler started")
-	defer fmt.Println("server: hello handler ended")
+func start(w http.ResponseWriter, req *http.Request) {
 
-	select {
-	case <-time.After(10 * time.Second):
-		fmt.Fprintf(w, "hello\n")
-	case <-ctx.Done():
+	// ctx := req.Context()
+	fmt.Println("[INFO]: connection received")
+	defer fmt.Println("[INFO]: responded")
 
-		err := ctx.Err()
-		fmt.Println("server:", err)
-		internalError := http.StatusInternalServerError
-		http.Error(w, err.Error(), internalError)
+	data := [][]int{
+		{0, 0, 0},
+		{0, 0, 0},
+		{0, 0, 0},
 	}
+	memory[i] = data
+	i += 1
+
+	fmt.Println("memory:", memory)
+
+	// select {
+	// case <-time.After(10 * time.Second):
+	// 	fmt.Fprintf(w, "hello\n")
+	// case <-ctx.Done():
+	// 	err := ctx.Err()
+	// 	fmt.Println("server:", err)
+	// 	internalError := http.StatusInternalServerError
+	// 	http.Error(w, err.Error(), internalError)
+	// }
 }
 
 func main() {
 
-	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/start", start)
 	http.ListenAndServe(":8090", nil)
 }
